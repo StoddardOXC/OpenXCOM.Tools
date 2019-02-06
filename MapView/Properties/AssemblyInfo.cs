7406f16edad43ef0b6fd7886df1b34705dca830d
kevL <k3v1s73n@gmail.com>
2019-02-05 21:24:42 -0700

    update vers.

--- MapView/Properties/AssemblyInfo.cs
+++ MapView/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Runtime.InteropServices;
 //	Build Number
 //	Revision
 //
-[assembly: AssemblyVersion ("2.7.0.0")]
-[assembly: AssemblyFileVersion ("2.7.0.0")]
+[assembly: AssemblyVersion ("2.8.0.0")]
+[assembly: AssemblyFileVersion ("2.8.0.0")]
 // satisfy FxCop:
 //[assembly: System.CLSCompliant(true)]
 // satisfy FxCop:
